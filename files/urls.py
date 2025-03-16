823de877c50a0457b686eb888f4ce10d8d6c2910
Pranjali <pranj1488@gmail.com>
2025-03-16 09:48:16 +0000

    Getting ready for lesson 9

--- files/urls.py
+++ files/urls.py
@@ -20,5 +20,6 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index)
+    path('', views.index, name = 'index'),
+    path('files/', views.files, name = 'files')
 ]
